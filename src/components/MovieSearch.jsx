635892a019126698fd2d23c966791384710800ec
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-11-28 18:11:51 +0100

    search refacer

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -9,9 +9,11 @@ const MovieSearch = () => {
 
 
     const API_KEY = '2dac3390988d579dbe536ee1ab9afbac'
-    const API_URL_MOVIES = 'https://api.themoviedb.org/3/search/movie'
-    const API_URL_TV = 'https://api.themoviedb.org/3/search/tv'
+    /*const API_URL_MOVIES = 'https://api.themoviedb.org/3/search/movie'
+    const API_URL_TV = 'https://api.themoviedb.org/3/search/tv'*/
     const IMAGE_URL = 'https://image.tmdb.org/t/p/w342/'
+
+    const API_URL = 'https://api.themoviedb.org/3/search/multi'
 
     const handleChange = (e) => {
         setQuery(e.target.value)
@@ -24,26 +26,15 @@ const MovieSearch = () => {
         setMovies([])
         setSeries([])
 
+        fetch(`${API_URL}?api_key=${API_KEY}&query=${query}&language=it`)
 
-        fetch(`${API_URL_MOVIES}?api_key=${API_KEY}&query=${query}&language=it`)
             .then(res => {
 
                 return res.json()
             })
 
             .then(data => {
-
                 setMovies(data.results)
-            })
-
-        fetch(`${API_URL_TV}?api_key=${API_KEY}&query=${query}&language=it`)
-
-            .then(res => {
-
-                return res.json()
-            })
-
-            .then(data => {
                 setSeries(data.results)
             })
     }
@@ -89,81 +80,48 @@ const MovieSearch = () => {
             </button>
 
             <div>
-                {movies.length > 0 || series.length > 0 ? (
-                    <>
-                        {movies.length > 0 && (
-                            <div>
 
-                                <h2>Film</h2>
-                                <ul>
-                                    {movies.map((movie) => (
-                                        <li key={movie.id}>
-                                            <img
-                                                src={IMAGE_URL + movie.poster_path}
-                                                alt={movie.title}
-                                                style={{ width: '150px', height: '225px' }}
-                                            />
-                                            <h3>{movie.title}</h3>
-                                            <p>
-                                                <strong>Titolo originale:</strong>
+                <>
 
-                                                {movie.original_title}
-                                            </p>
-                                            <p>
-                                                <strong>Lingua:</strong>
-                                                {langFlag(movie.original_language)}
-                                                {movie.original_language}
-                                            </p>
-                                            <p>
-                                                <strong>Voto:</strong>
-                                                {getRatingStars(movie.vote_average)}
-                                            </p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
+                    {movies.length > 0 ? (
+                        <div>
 
-                        {series.length > 0 && (
-                            <div>
-                                <h2>Serie TV</h2>
-                                <ul>
-                                    {series.map((serie) => (
+                            <h2>Risultati ricerca</h2>
+                            <ul>
+                                {movies.filter((movie) => movie.media_type !== "person").map((movie) => (
+                                    <li key={movie.id}>
+                                        <img
+                                            src={IMAGE_URL + movie.poster_path}
+                                            alt={movie.title || movie.name}
+                                            style={{ width: '150px', height: '225px' }}
+                                        />
+                                        <h3>{movie.title || movie.name}</h3>
+                                        <p>
+                                            <strong>Titolo originale:</strong>
 
-                                        <li key={serie.id}>
-                                            <img
-                                                src={IMAGE_URL + serie.poster_path}
-                                                alt={serie.name}
-                                                style={{ width: '150px', height: '225px' }}
-                                            />
-                                            <h3>{serie.name}</h3>
+                                            {movie.original_title}
+                                        </p>
+                                        <p>
+                                            <strong>Lingua:</strong>
+                                            {langFlag(movie.original_language)}
+                                            {movie.original_language}
+                                        </p>
+                                        <p>
+                                            <strong>Voto:</strong>
+                                            {getRatingStars(movie.vote_average)}
+                                        </p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ) : <div> Nessun risultato</div>
 
-                                            <p>
-                                                <strong>Titolo originale:</strong>
-                                                {serie.original_name}
+                    }
 
-                                            </p>
-                                            <p>
-                                                <strong>Lingua:</strong>
-                                                {langFlag(serie.original_language)}
-                                                {serie.original_language}
 
-                                            </p>
-                                            <p>
-                                                <strong>Voto:</strong>
-                                                {getRatingStars(serie.vote_average)}
+                </>
 
-                                            </p>
-                                        </li>
 
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-                    </>
-                ) : (
-                    <div> Nessun risultato</div>
-                )}
             </div>
         </div>
     )
